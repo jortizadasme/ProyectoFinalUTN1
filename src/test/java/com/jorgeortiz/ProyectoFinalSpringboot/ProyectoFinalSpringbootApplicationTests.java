5743a292ac7609056322bd3bc4060091d7e7ce1b
jortizadasme <jortiz.adasme@gmail.com>
2026-07-28 23:59:57 -0400

    commit v1.0
    primer commit

---
+++ src/test/java/com/jorgeortiz/ProyectoFinalSpringboot/ProyectoFinalSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeortiz.ProyectoFinalSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
